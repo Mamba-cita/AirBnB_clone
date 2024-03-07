bb5d804aab193b2af9a8db0b75024d704784a31a
gigi <gigi@gigis-MacBook-Air.local>
2024-03-07 13:08:15 +0300

    base/test

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -1,34 +1,53 @@
+#!/usr/bin/python3
 import unittest
 from models.base_model import BaseModel
 
-class TestBaseModel(unittest.TestCase):
-    def setUp(self):
-        self.base_model = BaseModel()
 
-    def test_attributes(self):
-        self.assertTrue(hasattr(self.base_model, 'id'))
-        self.assertTrue(hasattr(self.base_model, 'created_at'))
-        self.assertTrue(hasattr(self.base_model, 'updated_at'))
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        model = BaseModel()
+
+        self.assertIsNotNone(model.id)
+        self.assertIsNotNone(model.created_at)
+        self.assertIsNotNone(model.updated_at)
+
 
     def test_save(self):
-        original_updated_at = self.base_model.updated_at
-        self.base_model.save()
-        self.assertNotEqual(original_updated_at, self.base_model.updated_at)
+        model = BaseModel()
+
+        initial_update = model.updated_at
+        model.save()
+        updated_time = model.updated_at
+
+        self.assertGreater(updated_time, initial_update)
+
 
     def test_to_dict(self):
-        model_dict = self.base_model.to_dict()
+        model = BaseModel()
+
+        model_dict = model.to_dict()
         self.assertIsInstance(model_dict, dict)
         self.assertIn('__class__', model_dict)
         self.assertEqual(model_dict['__class__'], 'BaseModel')
-        self.assertIn('created_at', model_dict)
-        self.assertIn('updated_at', model_dict)
+        self.assertEqual(model_dict['id'], model.id)
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
 
     def test_str(self):
-        string_representation = str(self.base_model)
+        model = BaseModel()
+
+        string_representation = str(model)
         self.assertIsInstance(string_representation, str)
         self.assertIn('BaseModel', string_representation)
-        self.assertIn(self.base_model.id, string_representation)
-        self.assertIn(str(self.base_model.__dict__), string_representation)
+        self.assertIn(model.id, string_representation)
+        self.assertIn(str(model.__dict__), string_representation)
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
